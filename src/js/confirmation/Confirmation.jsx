d3ae66b7b3161704e9ffcdcdc1bd7fb813ae543a
Tyler Williams <tyler.williams@spothero.com>
2021-02-10 15:46:00 -0600

    Convert Confirmation to functional

--- src/js/confirmation/Confirmation.jsx
+++ src/js/confirmation/Confirmation.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {push} from 'connected-react-router';
@@ -12,66 +12,49 @@ const styles = {
     button: 'mt-12'
 };
 
-class Confirmation extends PureComponent {
-    static propTypes = {
-        selectedSpot: PropTypes.object,
-        pushTo: PropTypes.func.isRequired,
-        email: PropTypes.string.isRequired,
-    };
+const Confirmation = ({
+    email,
+    pushTo,
+    selectedSpot,
+}) => {
+    const handlePurchaseAnother = evt => pushTo('/');
 
-    constructor(props) {
-        super(props);
-
-        const {
-            selectedSpot,
-            pushTo
-        } = props;
-
-        // if you refresh on confirmation and there isn't a selectedSpot, make sure to go back to search and render nothing here
+    useEffect(() => {
         if (!selectedSpot) {
             pushTo('/');
         }
+    });
+
+    if (!selectedSpot) {
+        return null;
     }
 
-    _onPurchaseAnotherClick = evt => {
-        const {
-            pushTo,
-        } = this.props;
+    return (
+        <div className={styles.confirmation}>
+            <h1 className={styles.heading}>Park it like its hot!</h1>
+            <p >You successfully purchased parking at <strong>{selectedSpot.title}</strong> for <strong>${(selectedSpot.price / 100).toFixed(2)}</strong>.</p>
+            <Image
+                src={selectedSpot.image}
+                className={styles.image}
+            />
+            <p>We emailed a receipt to <a href={`mailto:${email}`}>{email}</a>.</p>
 
-        pushTo('/');
-    }
+            <Button
+                color="primary"
+                className={styles.button}
+                onClick={handlePurchaseAnother}
+            >
+                Purchase Another Spot!
+            </Button>
+        </div>
+    );
+};
 
-    render() {
-        const {
-            email,
-            selectedSpot
-        } = this.props;
-
-        if (!selectedSpot) {
-            return null;
-        }
-
-        return (
-            <div className={styles.confirmation}>
-                <h1 className={styles.heading}>Park it like its hot!</h1>
-                <p >You successfully purchased parking at <strong>{selectedSpot.title}</strong> for <strong>${(selectedSpot.price / 100).toFixed(2)}</strong>.</p>
-                <Image
-                    src={selectedSpot.image}
-                    className={styles.image}
-                />
-                <p>We emailed a receipt to <a href={`mailto:${email}`}>{email}</a>.</p>
-
-                <Button
-                    color="primary"
-                    className={styles.button}
-                    onClick={this._onPurchaseAnotherClick}
-                >
-                    Purchase Another Spot!
-                </Button>
-            </div>
-        );
-    }
-}
+Confirmation.propTypes = {
+    selectedSpot: PropTypes.object,
+    pushTo: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired,
+};
 
 const mapStateToProps = state => {
     const {
