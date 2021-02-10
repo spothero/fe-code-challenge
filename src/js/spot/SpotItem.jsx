dd0a06512b94cc4b9d93f03be78b32d390280c4c
Tyler Williams <tyler.williams@spothero.com>
2021-02-10 14:28:12 -0600

    SpotItem as functional

--- src/js/spot/SpotItem.jsx
+++ src/js/spot/SpotItem.jsx
@@ -1,6 +1,6 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+
 import Image from '../common/Image';
 import TextButton from '../common/TextButton';
 
@@ -9,50 +9,40 @@ const styles = {
     border: isSelected => `absolute top-0 left-0 bottom-0 w-2 z-10 ${isSelected ? 'bg-brand-green' : 'bg-transparent'}`
 };
 
-export default class SpotItem extends PureComponent {
-    static propTypes = {
-        showDetails: PropTypes.bool,
-        isSelected: PropTypes.bool,
-        data: PropTypes.object.isRequired,
-        onDetailsClick: PropTypes.func,
-    };
-    static defaultProps = {
-        showDetails: true,
-    };
+const SpotItem = ({data, isSelected, onDetailsClick, showDetails = true}) => {
+    const {
+        title,
+        image,
+        distance,
+    } = data;
 
-    _onDetailsClick = evt => {
-        const {data, onDetailsClick} = this.props;
+    const handleClick = () => onDetailsClick(data);
 
-        onDetailsClick(data);
-    };
-
-    render() {
-        const {
-            isSelected,
-            showDetails,
-            data: {image, distance, title},
-        } = this.props;
-        const classes = classNames(styles.item(isSelected), {
-            'SpotItem-selected': isSelected,
-        });
-
-        return (
-            <div className={classes}>
-                <div className={styles.border(isSelected)} />
-                <Image
-                    src={image}
-                    className="flex-shrink-0 w-56 h-36 border border-neutral-dashboard rounded"
-                />
-                <div className="ml-6 grid grid-rows-3 gap-row-4 items-center justify-items-start">
-                    <h2 className="text-3xl">{title}</h2>
-                    <p className="m-0">{distance}</p>
-                    {showDetails && (
-                        <TextButton onClick={this._onDetailsClick}>
-                            Details
-                        </TextButton>
-                    )}
-                </div>
+    return (
+        <div className={styles.item(isSelected)}>
+            <div className={styles.border(isSelected)} />
+            <Image
+                src={image}
+                className="flex-shrink-0 w-56 h-36 border border-neutral-dashboard rounded"
+            />
+            <div className="ml-6 grid grid-rows-3 gap-row-4 items-center justify-items-start">
+                <h2 className="text-3xl">{title}</h2>
+                <p className="m-0">{distance}</p>
+                {showDetails && (
+                    <TextButton onClick={handleClick}>
+                        Details
+                    </TextButton>
+                )}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+SpotItem.propTypes = {
+    isSelected: PropTypes.bool,
+    showDetails: PropTypes.bool,
+    onDetailsClick: PropTypes.func,
+    data: PropTypes.object.isRequired,
+};
+
+export default SpotItem;
